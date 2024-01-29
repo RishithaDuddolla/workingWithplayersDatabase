dba2ee40a1107144357b1a4e61d4dc8fed712e96
Rishitha <137534625+RishithaDuddolla@users.noreply.github.com>
2024-01-29 22:54:19 +0530

    Updated Put,Get apis

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ MakeserverDBConnection()
 app.get('/players/', async (request, response) => {
   const dbQuery = `
     SELECT * FROM cricket_team
-    ordered by player_id
+    order by player_id
     ;`
   const responsedb = await db.all(dbQuery)
   response.send(responsedb)
@@ -37,10 +37,10 @@ app.post('/players/', async (request, response) => {
   response.send('Player Added to Team')
 })
 app.get('/players/:playerId/', async (request, response) => {
-  const player_id = request.params
+  const playerId = request.params
   const dbQuery = `
     SELECT * FROM cricket_team
-    where player_id=${player_id}
+    where player_id=${playerId}
     ;`
   const responsedb = await db.get(dbQuery)
   response.send(responsedb)
@@ -49,17 +49,17 @@ app.put('/players/:player_id/', async (request, response) => {
   const playerdetails = request.body
   const playerId = request.params
   const {playerName, jerseyNumber, role} = playerdetails
-  const dbQuery = `UPDATE cricket_team (player_id, player_name, jersey_number,role) SET 
+  const dbQuery = `UPDATE cricket_team SET 
   (player_id=${playerId},player_name='${playerName}',jersey_number=${jerseyNumber},role='${role}')
   ;`
   const dbresponse = await db.run(dbQuery)
   response.send('Player Details Updated')
 })
 app.delete('/players/:playerId/', async (request, response) => {
-  const player_id = request.params
+  const playerId = request.params
   const dbQuery = `
     DELETE FROM cricket_team
-    where player_id=${player_id}
+    where player_id=${playerId}
     ;`
   const responsedb = await db.get(dbQuery)
   response.send('Player Removed')
